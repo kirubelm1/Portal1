90f2be16de11e7b28294a23d3c88ec9d9246da3a
Kirubel Mesfin <164068364+kirubelm1@users.noreply.github.com>
2024-09-12 13:36:39 +0300

    Add files via upload

---
+++ Forward Call/script.js
@@ -0,0 +1,122 @@
+//Agora Informations
+const APP_ID = "5b9b780b9f984ce0814390b8536e7a6c"
+const TOKEN = "007eJxTYNg5beqHq8m2Eq8OREX1uXTNOaF1n81z8dfLT7cpsCzlWbRDgcE0yTLJ3MIgyTLN0sIkOdXAwtDE2NIgycLU2CzVPNEs2ejwo7SGQEaGd2KfmBgZIBDEZ2HITczMY2AAANGDIUk="
+const CHANNEL = "main"
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+
+    client.on('user-published', handleUserJoined)
+    
+    client.on('user-left', handleUserLeft)
+    
+    let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks() 
+
+    let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+    
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+let joinStream = async () => {
+    await joinAndDisplayLocalStream()
+    document.getElementById('join-btn').style.display = 'none'
+    document.getElementById('stream-controls').style.display = 'flex'
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user 
+    await client.subscribe(user, mediaType)
+
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
+
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+    for(let i = 0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+
+    await client.leave()
+    document.getElementById('join-btn').style.display = 'block'
+    document.getElementById('stream-controls').style.display = 'none'
+    document.getElementById('video-streams').innerHTML = ''
+}
+
+let toggleMic = async (e) => {
+    if (localTracks[0].muted){
+        await localTracks[0].setMuted(false)
+        e.target.innerText = 'Mic on'
+        e.target.style.backgroundColor = 'cadetblue'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.innerText = 'Mic off'
+        e.target.style.backgroundColor = '#EE4B2B'
+    }
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.innerText = 'Camera on'
+        e.target.style.backgroundColor = 'cadetblue'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.innerText = 'Camera off'
+        e.target.style.backgroundColor = '#EE4B2B'
+    }
+}
+
+document.getElementById('join-btn').addEventListener('click', joinStream)
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+function updateStatus() {
+    const statusElement = document.querySelector('.status');
+    if (navigator.onLine) {
+        statusElement.textContent = 'You are online';
+        statusElement.classList.remove('offline');
+        statusElement.classList.add('online');
+    } else {
+        statusElement.textContent = 'No Internet Connection! Try again ';
+        statusElement.classList.remove('online');
+        statusElement.classList.add('offline');
+    }
+ }
+ 
+ // Initial check
+ updateStatus();
+ 
+ // Listen for changes in network status
+ window.addEventListener('online', updateStatus);
+ window.addEventListener('offline', updateStatus);
+ 
